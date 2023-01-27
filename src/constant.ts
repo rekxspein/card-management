7b6719e998d5a5546a391843f6a2d39cd1754464
rekxspein <khundrakpamrajesh95@gmail.com>
2023-01-27 12:43:36 +0530

    Loading Added, ReactQuery

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,7 @@
 const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
 
 export const BASE_API_URL = baseApiUrl;
+
+const mockapi = import.meta.env.VITE_REACT_APP_API;
+
+export const MOCKAPI = mockapi;
