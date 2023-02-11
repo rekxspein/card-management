c1c0f73cfcdf9bb1ba6ed3afc8be43d53a505984
rekxspein <khundrakpamrajesh95@gmail.com>
2023-02-11 14:35:07 +0530

    Crew List Initial Stage (only List shown)

--- src/store/customPage.ts
+++ src/store/customPage.ts
@@ -4,18 +4,22 @@ export const usePageNumber = create<
   ICustomPagination & ICustomPaginationActions
 >(set => ({
   pageNumber: 1,
-  pageSize: 10,
+  pageSize: 15,
+  total: 0,
 
   setPageNumber: (pageNo: number) => set({ pageNumber: pageNo }),
-  setPageSize: (size: number) => set({ pageSize: size })
+  setPageSize: (size: number) => set({ pageSize: size }),
+  setTotal: (size: number) => set({ total: size })
 }));
 
 export type ICustomPagination = {
   pageNumber: number;
   pageSize: number;
+  total: number;
 };
 
 type ICustomPaginationActions = {
   setPageNumber: (pageNo: number) => void;
   setPageSize: (size: number) => void;
+  setTotal: (size: number) => void;
 };
