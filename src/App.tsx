ed984157e5414a85ca7edbcd7ee741010d20db4f
rekxspein <khundrakpamrajesh95@gmail.com>
2023-03-02 17:57:10 +0530

    Download Feature Added for Crew List

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { FC, ReactNode } from 'react';
 import { QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
 import Page from './Page';
 import { queryClient } from './queryClient';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: FC = () => {
   return (
@@ -19,7 +21,20 @@ const ProviderWrapper: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <LocalizationProvider dateAdapter={AdapterMoment}>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <ThemeProvider theme={theme}>
+          {children}
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </ThemeProvider>
       </LocalizationProvider>
     </QueryClientProvider>
   );
