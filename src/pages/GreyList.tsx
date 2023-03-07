ffc95d94ee920bce67eb085ec54fbc02b4b5b723
rekxspein <khundrakpamrajesh95@gmail.com>
2023-03-07 20:51:37 +0530

    Setting Dynamic Download File Name for Declined Grey Cards

--- src/pages/GreyList.tsx
+++ src/pages/GreyList.tsx
@@ -4,6 +4,7 @@ import { DataGrid, GridActionsColDef, GridColDef } from '@mui/x-data-grid';
 import axios from 'axios';
 import { FC, useEffect } from 'react';
 import { useQuery } from 'react-query';
+import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { BASE_API_URL, AIRLINES } from '../constant';
 import { usePagination } from '../hooks/usePagination';
@@ -25,9 +26,13 @@ const getData = async (
 };
 
 export const GreyListPage: FC = () => {
-  const airlines = useActiveAirline(e => e.activeAirline);
+  const selectedAirlines = useActiveAirline(e => e.activeAirline);
   const airlinesIds = Object.keys(AIRLINES);
-  const mapping = airlines.map(a => airlinesIds.indexOf(a) + 1).join('');
+  const mapping = selectedAirlines
+    .map(a => airlinesIds.indexOf(a) + 1)
+    .join('');
+  const location = useLocation();
+  const fileName = location.pathname.replaceAll('/', '');
   const {
     page,
     query,
@@ -40,8 +45,9 @@ export const GreyListPage: FC = () => {
   } = usePagination({
     pageSize: 20
   });
-  const { data, isLoading } = useQuery(['getData', query, airlines], () =>
-    getData(airlines, query)
+  const { data, isLoading } = useQuery(
+    ['getData', query, selectedAirlines],
+    () => getData(selectedAirlines, query)
   );
 
   useEffect(() => {
@@ -92,8 +98,13 @@ export const GreyListPage: FC = () => {
 
                   const link = document.createElement('a');
                   link.href = href;
-                  const dwn = mapping === '1' ? 'Air-Asia' : 'Go-Airlines';
-                  link.setAttribute('download', `${dwn}.csv`);
+                  const selectedIdx = selectedAirlines.map(a => a);
+                  const downloadName =
+                    AIRLINES[selectedIdx.toString() as keyof typeof AIRLINES];
+                  link.setAttribute(
+                    'download',
+                    `${downloadName}-${fileName}.csv`
+                  );
                   document.body.appendChild(link);
                   link.click();
                   document.body.removeChild(link);
